5b1dfbce8d500d7d06c8860e667d1e0d34bb67d8
Faraz Razi <72218210+FarazRazi@users.noreply.github.com>
2023-09-19 11:25:37 +0500

    convert to Flask app

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
 def add(var1: int, var2: int):
     """Function to Add"""
     return var1 + var2
@@ -28,9 +33,33 @@ def modu(var1: int, var2: int):
 
     return var1 % var2
 
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    try:
+        data = request.json
+        choice = data['choice']
+        input1 = data['input1']
+        input2 = data['input2']
+
+        if choice == 1:
+            result = add(input1, input2)
+        elif choice == 2:
+            result = sub(input1, input2)
+        elif choice == 3:
+            result = mul(input1, input2)
+        elif choice == 4:
+            result = div(input1, input2)
+        elif choice == 5:
+            result = modu(input1, input2)
+        else:
+            return jsonify({'error': 'Invalid choice'}), 400
+
+        return jsonify({'result': result})
+
+    except (ValueError, KeyError, TypeError):
+        return jsonify({'error': 'Invalid input data'}), 400
+
+
 if __name__ == '__main__':
-    add(1,1)
-    sub(1,0)
-    mul(2,1)
-    div(4,2) 
-    modu(3,2)
+    app.run(host='0.0.0.0', debug=True, port=5000)
